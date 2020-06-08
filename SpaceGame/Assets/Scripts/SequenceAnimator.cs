57c04d5271f34f22f4c00fe70167aeb9ad43cb3b
hayoung298 <dudgk298@gmail.com>
2020-06-09 05:40:34 +0900

    -
    -

---
+++ SpaceGame/Assets/Scripts/SequenceAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceAnimator : MonoBehaviour
+{
+    public float waitTime = 0.15f;
+    public float waitEnd = 0.5f;
+    List<Animator> animators;
+
+    void Start()
+    {
+        animators = new List<Animator>(GetComponentsInChildren<Animator>());
+        StartCoroutine(Movement());
+    }
+
+    IEnumerator Movement()
+    {
+        while (true)
+        {
+            foreach(var animator in animators)
+            {
+                animator.SetTrigger("Movement");
+                yield return new WaitForSeconds(waitTime);
+            }
+            yield return new WaitForSeconds(waitEnd);
+        }
+    }
+}
