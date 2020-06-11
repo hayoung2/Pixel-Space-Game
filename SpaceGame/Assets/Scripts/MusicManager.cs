4d0a1e8de2108440795661c09226ac550624289f
hayoung298 <dudgk298@gmail.com>
2020-06-11 15:02:41 +0900

    Add music, LevelScene
    음악 추가 및 레벨 선택 씬 추가

---
+++ SpaceGame/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField]
+    private string name;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
