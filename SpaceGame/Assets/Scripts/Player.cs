47d5246b9f049a6a73ced4b38d0fc05cebef9c76
hayoung298 <dudgk298@gmail.com>
2020-06-16 08:18:56 +0900

    Add Player Animation
    player walk 구현

---
+++ SpaceGame/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed=1f;
+    private Rigidbody2D rigidbody;
+    private Animator animator;
+
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void Walk()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
+        }
+    }
+    
+    private void Jump()
+    {
+
+    }
+
+
+ 
+}
